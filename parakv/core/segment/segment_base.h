5d4ad323dc3df1b55715e1de5b5f80b859527e5f
liujinguang3 <jinguang.liu@gmail.com>
2026-04-13 14:13:07 +0800

    feat: update index design.

---
+++ parakv/core/segment/segment_base.h
@@ -0,0 +1,35 @@
+/* Copyright 2026 The ParaKV Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://gitcode.com/xLLM-AI/ParaKV/blob/main/LICENSE
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#pragma once
+
+#include <string>
+
+namespace parakv {
+
+namespace segment {
+
+class SegmentBase {
+public:
+    SegmentBase();
+    virtual ~SegmentBase();
+
+    virtual void Compact() = 0;
+    virtual void Insert(const std::string& key, const std::string& value) = 0;
+    
+    };
+} // namespace segment
+
+} // namespace parakv
